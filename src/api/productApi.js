8de85f77719ce0bd8ff6e006602e7f90bb8bb8ea
heyskarim <saadkarim145@gmail.com>
2025-12-15 10:31:44 +0100

    first commit

---
+++ src/api/productApi.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const api = axios.create({
+	// baseURL: 'https://gestion-produits-backend-production.up.railway.app/api',
+	baseURL: "http://127.0.0.1:8000/api",
+});
+
+export const getProducts = () => api.get("/products");
+export const createProduct = (data) => api.post("/products", data);
+export const updateProduct = (id, data) => api.put(`/products/${id}`, data);
+export const deleteProduct = (id) => api.delete(`/products/${id}`);
+
+export default api;
